cbd8e097cbdf37e3ab6f711b342095410b73a659
Cristopher Dávila <cristopher.d.96@gmail.com>
2019-11-11 02:58:24 -0500

    init commit

---
+++ WhatsApp_Twilio/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for WhatsApp_Twilio project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/1.11/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "WhatsApp_Twilio.settings")
+
+application = get_wsgi_application()
---
+++ twilioApp/views.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.shortcuts import render
+from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+from twilio.rest import Client
+
+# Create your views here.
+@csrf_exempt
+def tempHumData(request):
+    # Your Account Sid and Auth Token from twilio.com/console
+  
+    account_sid = 'AC26f748d29bce5289dfbe6ea9d2e9faee'
+  
+    auth_token = '3a8fc6e6a07774fde54ab518b1c20743'
+  
+    client = Client(account_sid, auth_token)
+    
+    # temperature = request.POST['']
+
+    data = {
+        'temperature': request.POST,
+        'humidity': 'humidity'
+    }
+
+    # client.messages.create(
+    #     body='Temperature: ' + temperature + '\nHumidity: ' + humidity,
+    #     from_='whatsapp:+14155238886',
+    #     to='whatsapp:+593979103931')
+
+    return JsonResponse(data, safe=False)
