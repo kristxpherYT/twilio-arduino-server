41c8a84303740b5d30e1b60bb520f89a939f958a
Cristopher Dávila <cristopher.d.96@gmail.com>
2019-11-11 05:20:26 -0500

    fixes

--- WhatsApp_Twilio/settings.py
+++ WhatsApp_Twilio/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'twilioApp.apps.TwilioAppConfig'
+    'twilioApp.apps.TwilioappConfig'
 ]
 
 MIDDLEWARE = [
